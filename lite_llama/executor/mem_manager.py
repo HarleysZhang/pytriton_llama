18e23346379c4f455b913c311dfa7e805d6091c2
HarleysZhang <zhg5200211@outlook.com>
2024-11-25 22:26:11 +0800

    fix: available_kv_cache_memory and peak_torch_memory

--- lite_llama/executor/mem_manager.py
+++ lite_llama/executor/mem_manager.py
@@ -1,5 +1,5 @@
 import torch
-import logging 
+import logging, gc
 from typing import List
 from ..models.model_config import LlamaConfig
 
@@ -27,7 +27,7 @@ class ComputeMaxAvailableBlocks:
         else:
             print(f"Unsupported dtype: {self.dtype_size}!")
         
-    def compute_cache_block_size_bytes(self,):
+    def compute_cache_block_size_bytes(self):
         """Get the size of the KV cache block size in bytes.
         """
         if self.model_config.head_dim is None:
@@ -46,45 +46,75 @@ class ComputeMaxAvailableBlocks:
         return transformer_kv_cache_blocks_bytes
 
     def compute_num_available_blocks(self, model_path=None, dummy_input = None, model_byes=None):
-        free_memory, total_gpu_memory = torch.cuda.mem_get_info()
-        peak_memory = torch.cuda.memory_stats()["allocated_bytes.all.peak"] # 获取峰值内存分配
+        """
+        评估模型的峰值内存使用情况，以确定在不发生内存溢出的情况下可以分配的 KV（键值）缓存块的数量。
+
+        该方法首先清理 CUDA 缓存，然后使用虚拟输入执行一次前向传播，以评估模型的内存使用情况。
+        接着，计算在剩余可用内存下，最多可以分配的 GPU 和 CPU 缓存块数量。
+
+        提示：
+            可以通过调整 `gpu_memory_utilization` 参数来限制 GPU 内存的使用。
+        """
+        # 清理 CUDA 缓存，以确保获取准确的内存使用信息
+        # NOTE: torch.cuda.empty_cache() 用于释放 GPU 上由缓存分配器持有的未占用内存。
+        # NOTE: torch.cuda.reset_peak_memory_stats() 用于重置 CUDA 内存分配器所跟踪的“峰值”统计数据。
+        torch.cuda.empty_cache()
+        torch.cuda.reset_peak_memory_stats()
+
+        # 获取当前 GPU 的空闲内存和总内存（单位：字节）
+        free_memory_pre_profile, total_gpu_memory = torch.cuda.mem_get_info()
+
+        # TODO: 使用虚拟输入执行一次前向传播，以评估模型的内存使用情况
+
+        # 同步 CUDA 操作，确保内存信息准确
+        torch.cuda.synchronize()
+        # 计算模型加载后的峰值内存使用量. Get the peak memory allocation recorded by torch
+        peak_memory = torch.cuda.memory_stats()["allocated_bytes.all.peak"]
         
-        # 清理未使用的缓存，计算非Torch分配的内存
+        # 清理未使用的缓存，计算非 Torch 分配的内存. 检查是否有任何剩余内存可能已在“torch”之外的 gpu 上分配。例如，NCCL 操作在前向传递期间可能会使用几 GB
         torch.cuda.empty_cache()
         torch_allocated_bytes = torch.cuda.memory_stats()["allocated_bytes.all.current"]
-        total_allocated_bytes = total_gpu_memory - free_memory
+        
+        total_allocated_bytes = torch.cuda.mem_get_info()[1] - torch.cuda.mem_get_info()[0]
         non_torch_allocations = total_allocated_bytes - torch_allocated_bytes
-
-        # 如果有非Torch分配的内存，增加到峰值内存中
+        
         if non_torch_allocations > 0:
             peak_memory += non_torch_allocations
 
-        # 计算可用于KV缓存的内存, 单位 字节
         available_kv_cache_memory = (
             total_gpu_memory * self.gpu_memory_utilization -
-            peak_memory
-        )
-        # if available_kv_cache_memory <= 0:
-        # available_kv_cache_memory = free_memory
+            peak_memory)
         
-        cache_block_size = self.compute_cache_block_size_bytes() # 单位字节
+        # 计算每个缓存块的大小
+        cache_block_size = self.compute_cache_block_size_bytes()
+        # 计算在剩余可用内存下，最多可以分配的 GPU 缓存块数量
+        num_gpu_blocks = int(
+            (total_gpu_memory * self.gpu_memory_utilization -
+            peak_memory) // cache_block_size
+        )
+        # 确保缓存块数量不为负数
+        num_gpu_blocks = max(num_gpu_blocks, 0)
 
-        if cache_block_size == 0:
-            max_gpu_num_blocks = 0
-        else:
-            max_gpu_num_blocks = int(available_kv_cache_memory // cache_block_size)
-        
         logger.info(
-            f"cache_block_size = {cache_block_size / (1024 ** 2)} MB \n"
-            f"Memory profiling result: total_gpu_memory={total_gpu_memory / (1024**3):.2f} GB, \n"
-            f"peak_torch_memory={(peak_memory - non_torch_allocations) / (1024**3):.2f} GB, \n"
-            f"non_torch_memory={non_torch_allocations / (1024**3):.2f} GB, \n"
-            f"kv_cache_size={available_kv_cache_memory / (1024**3):.2f} GB, \n"
-            f"available_kv_cache_memory={available_kv_cache_memory / (1024**3):.2f} GB, \n"
-            f"max_gpu_num_blocks={max_gpu_num_blocks}\n"
-        )
-    
-        return max_gpu_num_blocks
+                " Memory profiling results: total_gpu_memory = %.2f GB \n"
+                " initial_memory_usage = %.2f GB peak_torch_memory = %.2f GB \n"
+                " memory_usage_post_profile = %.2f GB \n"
+                " non_torch_memory = %.2f GB, kv_cache_size = %.2f GB \n"
+                " gpu_memory_utilization = %.2f", total_gpu_memory / (1024**3),
+                (total_gpu_memory - free_memory_pre_profile) / (1024**3),
+                (peak_memory - non_torch_allocations) / (1024**3),
+                total_allocated_bytes / (1024**3),
+                non_torch_allocations / (1024**3),
+                available_kv_cache_memory / (1024**3),
+                self.gpu_memory_utilization)
+
+        # 进行垃圾回收，释放未使用的内存
+        gc.collect()
+        # 再次清理 CUDA 缓存
+        torch.cuda.empty_cache()
+        # 返回可分配的 GPU 和 CPU 缓存块数量（此处 CPU 块数量为 0）
+
+        return num_gpu_blocks
     
 
 class KVCacheMemoryManager:
@@ -132,7 +162,7 @@ class KVCacheMemoryManager:
         
         can_use_pos_index = torch.nonzero(self.kv_mem_use_state == 0).view(-1)
         select_index = can_use_pos_index[0:need_size]
-        self.add_refs(select_index)
+        self.add_ref(select_index)
         
         return select_index
     
@@ -162,13 +192,13 @@ class KVCacheMemoryManager:
             start_index = start_indexs[contiguous_blocks[0]].item() # item 方法将张量转换成 Python 数值
             end_index = start_index + need_size
             select_index = self.kv_mem_pos_indexs[start_index:end_index]
-            self.add_refs(select_index)
+            self.add_ref(select_index)
             return select_index, start_index, end_index
 
         return None
     
     @torch.no_grad()
-    def add_refs(self, token_index: torch.Tensor):
+    def add_ref(self, token_index: torch.Tensor):
         state = self.kv_mem_use_state[token_index]
         has_used_tokens = torch.count_nonzero(state).item()
         all_tokens = len(state)
@@ -178,7 +208,7 @@ class KVCacheMemoryManager:
         return
     
     @torch.no_grad()
-    def decrease_refs(self, token_index: torch.Tensor):
+    def release_ref(self, token_index: torch.Tensor):
         # 使用 unique 方法获取 token_index 中唯一的 token 索引，并返回每个唯一索引在原始张量中出现的次数。
         token_index, counts = token_index.unique(return_counts=True)
         # 当引用计数减少到零时，意味着该缓存块可以被释放或重新分配。

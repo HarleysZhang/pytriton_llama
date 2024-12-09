fix: softmax triton kernels

lite_llama/generate.py
@@ -5,9 +5,8 @@ from transformers import AutoTokenizer
 
 from .executor.model_executor import ModelExecutor
 from .utils.file_interface import get_model_name_from_path
-from .kernels.softmax import softmax_fwd
-from .kernels.softmax_online_v2 import softmax_split
-
+from .kernels.softmax_split import softmax_split
+.kernels.softmax_split
 class CompletionPrediction(TypedDict, total=False):
     generation: str
     tokens: List[str]  # not required
@@ -122,16 +121,10 @@ class GenerateText:
         
         for cur_pos in range(max_prompt_len, total_len):
             input_ids = tokens[:, prev_pos: cur_pos] # 当前输入 token ids, decode 阶段 input_ids shape is [4, 1]
-            logits, select_index = self.model_executor.forward(input_ids, prev_pos) # 模型执行器的前向推理
-            print("logits shape is ", logits.shape)
-            assert not torch.isnan(logits).any(), f"In pos: {cur_pos}, Model forward output logits tensor contains NaN values!"
-
-            if temperature > 0:
-                probs = softmax_split(logits[:, -1] / temperature) # torch.softma 将 logits 转换为概率分布。
-                assert not torch.isnan(probs).any(), f"In pos: {cur_pos}, logits probs tensor contains NaN values!"
-                next_token = sample_top_p(probs, top_p) # next_token 形状为 [batch_size, 1]
-            else:
-                next_token = torch.argmax(logits[:, -1], dim=-1)
+            logits, select_index = self.model_executor.forward(input_ids, prev_pos) # 模型执行器的前向推理, logits shape is [batch_size, shape, vocab_size]
+            
+            probs = softmax_split(logits[:, -1] / temperature) # torch.softma 将 logits 转换为概率分布。
+            next_token = sample_top_p(probs, top_p) # next_token 形状为 [batch_size, 1]
 
             next_token = next_token.reshape(-1) # 调整为一维, shape is batch_size
             

lite_llama/kernels/softmax_native.py
@@ -68,11 +68,11 @@ def _softmax_kernel_fwd(
     tl.store(output_pointers, softmax_out, mask = row_data_mask)
 
 @torch.no_grad()
-def softmax_fwd(x: torch.Tensor) -> torch.Tensor:
+def softmax_native_fwd(x: torch.Tensor) -> torch.Tensor:
     """Triton impl of Softmax, onlay support 2D tensor in fwd"""
     rows, cols = x.shape
     assert x.ndim == 2, f"only accepts 2D tensor now"
-    BLOCK_SIZE = min(65536, triton.next_power_of_2(cols))
+    BLOCK_SIZE = triton.next_power_of_2(cols)
     num_warps = 4
     if BLOCK_SIZE >= 32768:
         num_warps = 32

lite_llama/kernels/softmax_split.py
@@ -17,7 +17,7 @@ def logsumexp_kernel(
     n_offsets = pid_n * TILE_N + tl.arange(0, TILE_N)
     mask = n_offsets < N
     offset = pid_m * N + n_offsets
-    inp = tl.load(in_ptr + offset, mask=mask, other=-float("inf")).to(out_ptr.dtype.element_ty)
+    inp = tl.load(in_ptr + offset, mask=mask, other=-float("inf")).to(tl.float32)
     m = tl.max(inp, 0)
     e = tl.exp(inp - m)
     z = tl.sum(e, 0)
@@ -46,13 +46,12 @@ def softmax_kernel(out_ptr, in_ptr, logz_ptr, M, N, TILE_N: tl.constexpr):
     offset = pid_m * N + n_offsets
     mask = n_offsets < N
     inp = tl.load(in_ptr + offset, mask=mask, other=-float("inf")).to(out_ptr.dtype.element_ty)
-    logz = tl.load(logz_ptr + pid_m).to(out_ptr.dtype.element_ty)
+    logz = tl.load(logz_ptr + pid_m).to(tl.float32)
     out = tl.exp(inp - logz)
     tl.store(out_ptr + offset, out, mask=mask)
 
 
-
-def softmax_onlinev2(x):
+def softmax_split(x):
     M, N = x.shape
 
     num_sms = torch.cuda.get_device_properties(x.device).multi_processor_count